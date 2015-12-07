52d08b8f7ec4992dd63b93fe87dbb084922a57f3
goodhead4media <goodheadmedia@outlook.com>
2015-12-07 13:12:36 +0000

    Update breakeven.js

--- breakeven.js
+++ breakeven.js
@@ -1,19 +1,15 @@
 /* Break-Even.js */
-
 // There are only two functions that interact with the page: getNumbers() and output();
 function getNumbers() {
      return {
         "survival": $("#txtSurvival").val(),
-         "product": $("#txtProduct").val()
+        "product": $("#txtProduct").val()
     };
 }
- 
 // Divides the product cost by the survival budget
 function calc(nums) {
     return (nums.survival / nums.product);
 }
- 
 function output(result) {
     $("#lblResult").html("The number of products you'd have to produce in the year is: <br/>" + result);
 }
- 
